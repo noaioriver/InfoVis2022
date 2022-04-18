761c1af34e821159ebc502baa69adc2a988b2125
Noa Ogawa <noa@NoanoMacBook-Air.local>
2022-04-18 20:13:58 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,24 +1,11 @@
-Vec3 = function( x, y, z )
+class Vec3 
 {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-}
-
-Vec3.prototype.add = function( v )
-{
-    this.x += v.x;
-    this.y += v.y;
-    this.z += v.z;
-    return this;
-}
-
-Vec3.prototype.sub = function( v )
-{
-    this.x -= v.x;
-    this.y -= v.y;
-    this.z -= v.z;
-    return this;
+    constructor(x,y,z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z; 
+    }
 }
 
 Vec3.prototype.sum = function()
@@ -44,16 +31,4 @@ Vec3.prototype.mid = function()
     return this.sum() - this.min() - this.max();
 }
 
-Vec3.prototype.cross = function( v )
-{
-    var x = this.x, y = this.y, z = this.z;
-    this.x = y * v.z - z * v.y;
-    this.y = z * v.x - x * v.z;
-    this.z = x * v.y - y * v.x;
-    return this;
-}
 
-Vec3.prototype.length = function()
-{
-    return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
-}
