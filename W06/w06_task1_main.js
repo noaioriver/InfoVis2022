f1723cf9402018418a3878f343354bc4b46e340a
Noa Ogawa <noa@NoanoMacBook-Air.local>
2022-05-09 02:55:19 +0900

    Initial commit

---
+++ W06/w06_task1_main.js
@@ -0,0 +1,116 @@
+d3.csv("https://noaioriver.github.io/InfoVis2022/W06/w06_task2.csv")
+    .then( data => {
+        data.forEach( d => { d.x = +d.x; d.y = +d.y; });
+
+        var config = {
+            parent: '#drawing_region',
+            width: 256*2,
+            height: 256*2,
+            margin: {top:30, right:30, bottom:30, left:30}
+        };
+
+        const scatter_plot = new ScatterPlot( config, data );
+        scatter_plot.update();
+    })
+    .catch( error => {
+        console.log( error );
+    });
+
+class ScatterPlot {
+
+    constructor( config, data ) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 256,
+            margin: config.margin || {top:20, right:20, bottom:20, left:20}
+        }
+        this.data = data;
+        this.init();
+    }
+
+    init() {
+        let self = this;
+
+        self.svg = d3.select( self.config.parent )
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+
+        self.mrg_inner_width=self.inner_width-40;
+        self.mrg_inner_height=self.inner_height-40;
+
+        self.xscale = d3.scaleLinear()
+            .range( [0, self.inner_width] );
+
+        self.yscale = d3.scaleLinear()
+            .range( [self.inner_height, 0] );
+        
+        self.rexscale=d3.scaleLinear()
+            .range([0,self.mrg_inner_width]);
+
+        self.reyscale=d3.scaleLinear()
+            .range([0,self.mrg_inner_height]);
+
+        self.xaxis = d3.axisBottom( self.xscale )
+            .ticks(10)//.tickFormat((d)=>{return d*10+1+'ŒŽ'});
+            .tickFormat(d => d === 0  || d===13? "" : d+"ŒŽ")
+
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`);
+
+        self.yaxis = d3.axisLeft(self.yscale)
+            .ticks(10)
+            .tickFormat(d=> d===30?d+"Ž":d)
+            
+
+        self.yaxis_group=self.chart.append('g')
+    }
+
+    update() {
+        let self = this;
+
+        //const xmin = d3.min( self.data, d => d.x );
+        //const xmax = d3.max( self.data, d => d.x );
+
+        const xmin=0;
+        //let xmax=d3.max( self.data, d => d.x );
+        //xmax=xmax+20;
+        const xmax=13;
+
+
+        self.xscale.domain( [xmin, xmax] );
+
+        //const ymin = d3.min( self.data, d => d.y );
+        //const ymax = d3.max( self.data, d => d.y );
+        const ymax=30;
+        const ymin=0;
+        self.yscale.domain( [ymin, ymax] );
+
+        self.render();
+    }
+
+    render() {
+        let self = this;
+
+        self.chart.selectAll("circle")
+            .data(self.data)
+            .enter()
+            .append("circle")
+            .attr("cx", d => self.xscale( d.x ) )
+            .attr("cy", d => self.yscale( d.y ) )
+            .attr("r", d => d.r )
+            //.attr('transform',`translate(${self.config.margin.left},0)`)
+            
+        self.xaxis_group
+            .call( self.xaxis );
+        
+        self.yaxis_group
+            .call(self.yaxis);
+    }
+}
